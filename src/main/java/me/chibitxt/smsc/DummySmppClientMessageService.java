767b1b7d4be80546d65cdc8ceeb2044da57bd9b1
David Wilkie <dwilkie@gmail.com>
2015-02-22 16:23:40 +0700

    Add println debugging for MO or delivery receipt

--- src/main/java/me/chibitxt/smsc/DummySmppClientMessageService.java
+++ src/main/java/me/chibitxt/smsc/DummySmppClientMessageService.java
@@ -8,6 +8,7 @@ public class DummySmppClientMessageService implements SmppClientMessageService {
   /** delivery receipt, or MO */
   @Override
   public PduResponse received(OutboundClient client, DeliverSm deliverSm) {
+    System.out.println("------------------RECEIVED Delivery receipt or MO-----------------");
     return deliverSm.createResponse();
   }
 }
